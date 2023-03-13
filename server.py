d3893b039e63fee25299c027ca43441fb2370b48
Jocelyn Shen <joceshen@mit.edu>
2023-03-13 12:09:59 -0400

    firebase and server template code

--- server.py
+++ server.py
@@ -34,69 +34,95 @@ num_hits_per_worker = 15
 def hello_world():
     return "Hello World"
 
-@app.route('/getIp/', methods=["GET", "POST"])
-def get_ip():
-    lock.acquire()
-    try:
-        print(request.form)
-        ip = request.form.get("ipaddress")
-        if ip:
-            ip = ip.replace(".", "-")
-        dbsource=request.form.get("dbsource")
-        workerId = request.form.get("workerId")
-        ref = db.reference(f'/{dbsource}/') 
-        current_ids = ref.get()
-        print(f"GET REQ FROM {ip}, {workerId}")
-        if ip in current_ids:
-            ip_count = current_ids[ip]
-        else:
-            ip_count = 0
-        if workerId in current_ids:
-            worker_count = current_ids[workerId]
-        else:
-            worker_count = 0
+@app.route('/getSessionNumber/', methods=["GET", "POST"])
+def get_session_number():
+    """Get current session number for participant"""
+    # randomly select story FROM stories that haven't been seen before (store it in firebase)
+    pass
 
-        if ip_count >= num_hits_per_worker or worker_count >= num_hits_per_worker:
-            donehit = True 
-        else:
-            donehit = False
-        return json.dumps(dict(donehit=donehit))
-    except:
-        return abort(404)
-    finally:
-        lock.release()
 
-@app.route('/uploadIp/', methods=["GET", "POST"])
-def upload_ip():
-    lock.acquire()
-    try:
-        print(request.form)
-        ip = request.form.get("ipaddress")
-        if ip:
-            ip = ip.replace(".", "-")
-        workerId = request.form.get("workerId")
-        dbsource=request.form.get("dbsource")
-        ref = db.reference(f'/{dbsource}/')
-        current_ids = ref.get()
-        if ip in current_ids:
-            ip_count = current_ids[ip] + 1
-        else:
-            ip_count = 1
-        if workerId in current_ids:
-            worker_count = current_ids[workerId] + 1
-        else:
-            worker_count = 1
-        if ip:
-            ref.update({ip:ip_count})
-        if workerId:
-            ref.update({workerId: worker_count})
-            ref.update({workerId + "_to_ip": ip})
-        return json.dumps(dict(success=True))
-    except Exception as e:
-        print(e)
-        return abort(404)
-    finally:
-        lock.release()
+@app.route('/getPrompt/', methods=["GET", "POST"])
+def get_prompt():
+    """Get initial writing prompt for user + save to firebase"""
+    # randomly select story FROM stories that haven't been seen before (store it in firebase)
+    pass
+
+@app.route('/getStories/', methods=["GET", "POST"])
+def get_stories():
+    """Get 3 stories from the model + save to firebase"""
+    
+    return json.dumps(dict(story1, "my story is great"))
+
+@app.route('/submit/', methods=["GET", "POST"])
+def submit():
+    """User submitted survey/demographic information + save their data to firebase"""
+    pass
+
+
+
+# @app.route('/getIp/', methods=["GET", "POST"])
+# def get_ip():
+#     lock.acquire()
+#     try:
+#         print(request.form)
+#         ip = request.form.get("ipaddress")
+#         if ip:
+#             ip = ip.replace(".", "-")
+#         dbsource=request.form.get("dbsource")
+#         workerId = request.form.get("workerId")
+#         ref = db.reference(f'/{dbsource}/') 
+#         current_ids = ref.get()
+#         print(f"GET REQ FROM {ip}, {workerId}")
+#         if ip in current_ids:
+#             ip_count = current_ids[ip]
+#         else:
+#             ip_count = 0
+#         if workerId in current_ids:
+#             worker_count = current_ids[workerId]
+#         else:
+#             worker_count = 0
+
+#         if ip_count >= num_hits_per_worker or worker_count >= num_hits_per_worker:
+#             donehit = True 
+#         else:
+#             donehit = False
+#         return json.dumps(dict(donehit=donehit))
+#     except:
+#         return abort(404)
+#     finally:
+#         lock.release()
+
+# @app.route('/uploadIp/', methods=["GET", "POST"])
+# def upload_ip():
+#     lock.acquire()
+#     try:
+#         print(request.form)
+#         ip = request.form.get("ipaddress")
+#         if ip:
+#             ip = ip.replace(".", "-")
+#         workerId = request.form.get("workerId")
+#         dbsource=request.form.get("dbsource")
+#         ref = db.reference(f'/{dbsource}/')
+#         current_ids = ref.get()
+#         if ip in current_ids:
+#             ip_count = current_ids[ip] + 1
+#         else:
+#             ip_count = 1
+#         if workerId in current_ids:
+#             worker_count = current_ids[workerId] + 1
+#         else:
+#             worker_count = 1
+#         if ip:
+#             ref.update({ip:ip_count})
+#         if workerId:
+#             ref.update({workerId: worker_count})
+#             ref.update({workerId + "_to_ip": ip})
+#         return json.dumps(dict(success=True))
+#     except Exception as e:
+#         print(e)
+#         return abort(404)
+#     finally:
+#         lock.release()
 
 
 ################################### START SERVER ###################################
